b6a573930539fa4fcc0bd62883c90e2eb1d96470
Sithija Nelusha Silva <hello@snelusha.dev>
2026-03-20 23:20:58 +0530

    Implement file route synchronization

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,13 +2,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 
 import { FSProvider } from "@/providers/fs-provider";
 
+import { FileRouteSync } from "@/components/file-route-sync";
 import { Editor } from "@/components/editor";
 
 export default function App() {
 	return (
 		<TooltipProvider>
 			<FSProvider>
-				<Editor />
+				<FileRouteSync>
+					<Editor />
+				</FileRouteSync>
 			</FSProvider>
 		</TooltipProvider>
 	);
